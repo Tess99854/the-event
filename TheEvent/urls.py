5cd4bc755a79151718395327c3fe62c66fd3d9df
Tess99854 <stesnime@yahoo.com>
2022-05-12 09:19:19 +0100

    first commit

---
+++ TheEvent/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from . import views
+
+urlpatterns = [
+    path('login', views.TokenObtainCustom.as_view(), name='token_obtain'),
+    path('refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register', views.register, name="register"),
+    path('tickets', views.get_tickets, name="tickets"),
+    path('add_ticket', views.add_ticket, name="add_ticket"),
+]
